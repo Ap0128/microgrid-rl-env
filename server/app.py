a789401a1a72bdd45c7dbbf05271a12e487c7fc0
Anirudh Patil <patilanirudha124@gmail.com>
2026-04-09 23:18:14 +0530

    server app fixed

--- server/app.py
+++ server/app.py
@@ -1,1 +1,17 @@
-from main import app
+import uvicorn
+from env.microgrid import MicrogridEnv
+from openenv.core.env_server import create_app
+from env.models import MicrogridState, MicrogridAction, StepRecord, StepResult, GradeResult
+
+app = create_app(
+    MicrogridEnv,
+    action_type = MicrogridAction,
+    observation_type=MicrogridState,
+    env_name="microgrid_v1"
+)
+
+def main():
+    uvicorn.run(app, host="0.0.0.0", port=8000)
+
+if __name__ == "__main__":
+    main()
